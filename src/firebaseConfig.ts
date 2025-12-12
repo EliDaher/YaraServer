d9ee0f7ccbeb1706395f8c2c1631b638fac2a7ed
elidaher <eeli56315@gmail.com>
2025-12-12 14:38:52 +0300

    fix firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -6,22 +6,22 @@ import dotenv from "dotenv";
 dotenv.config(); // تحميل متغيرات البيئة
 
 const firebaseConfig = {
-  // apiKey: process.env.FIREBASE_apiKey,
-  // authDomain: process.env.FIREBASE_authDomain,
-  // databaseURL: process.env.FIREBASE_databaseURL,
-  // projectId: process.env.FIREBASE_projectId,
-  // storageBucket: process.env.FIREBASE_storageBucket,
-  // messagingSenderId: process.env.FIREBASE_messagingSenderId,
-  // appId: process.env.FIREBASE_appId,
-  // measurementId: process.env.FIREBASE_measurementId,
-  apiKey: "AIzaSyDwZmm9YFxUsXaNGadxNWyDa3sN7DCDnNs",
-  authDomain: "fitnesstime-4096f.firebaseapp.com",
-  databaseURL: "https://fitnesstime-4096f-default-rtdb.firebaseio.com",
-  projectId: "fitnesstime-4096f",
-  storageBucket: "fitnesstime-4096f.firebasestorage.app",
-  messagingSenderId: "577921419371",
-  appId: "1:577921419371:web:6b1bced7ebc17a07b5092e",
-  measurementId: "G-MB1NYNBY2M",
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  databaseURL: process.env.FIREBASE_databaseURL,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId,
+  measurementId: process.env.FIREBASE_measurementId,
+  // apiKey: "AIzaSyDwZmm9YFxUsXaNGadxNWyDa3sN7DCDnNs",
+  // authDomain: "fitnesstime-4096f.firebaseapp.com",
+  // databaseURL: "https://fitnesstime-4096f-default-rtdb.firebaseio.com",
+  // projectId: "fitnesstime-4096f",
+  // storageBucket: "fitnesstime-4096f.firebasestorage.app",
+  // messagingSenderId: "577921419371",
+  // appId: "1:577921419371:web:6b1bced7ebc17a07b5092e",
+  // measurementId: "G-MB1NYNBY2M",
 };
 
 const app = initializeApp(firebaseConfig);
